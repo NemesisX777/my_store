44052a6b145d4bfdd29daa4f2a972a83b95b088c
Roman Mochalov <r.mochalov@gmail.com>
2023-02-08 13:09:22 +0300

    Вариант с рабочей таблицей на Vue.js

--- static/vue_app.js
+++ static/vue_app.js
@@ -1,21 +1,32 @@
-new Vue ({
-    "el": '#products_app',
-    "data": {
-        "products": [],
-        "categories": []
+var ExampleComponent = new Vue({
+    el: "#VueProductTable",
+    data: {
+        selectedValue: "",
+        data: [],
+        options: []
     },
-    "created": function () {
-        const vm_1 = this;
-        axios.get('http://127.0.0.1:8000/api/products/')
-            .then(function (response) {
-                console.log(response.data)
-                vm_1.products = response.data
-            })
-        const vm_2 = this;
-        axios.get('http://127.0.0.1:8000/api/categories/')
-            .then(function (response) {
-                console.log(response.data)
-                vm_2.categories = response.data
-            })
+    methods: {
+        fetchData: function () {
+            axios.get("http://127.0.0.1:8000/api/categories/")
+                .then(response => {
+                    this.options = response.data;
+                });
+        },
+        fetchTableData: function (value = 8) {
+            axios.get(`http://127.0.0.1:8000/api/categories/${value}/products/`)
+                .then(response => {
+                    this.data = response.data;
+                });
+        }
+    },
+    created: function () {
+        this.fetchData();
+    },
+    watch: {
+        selectedValue: function (value) {
+            if (value) {
+                this.fetchTableData(value);
+            }
+        }
     }
-})
+});
